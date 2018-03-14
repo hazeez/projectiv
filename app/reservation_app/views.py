614004a2d1bc77ce209e9a3818d3b140118ca55e
HAZEEZ <HAZEEZ@HAZEEZ-IN.in.oracle.com>
2018-03-14 18:38:33 +0530

    working version - sign in barring the submit btn triggering

--- app/reservation_app/views.py
+++ app/reservation_app/views.py
@@ -7,6 +7,7 @@ from flask_migrate import Migrate
 import pyglet, os
 from app import app, db, login
 
+
 #import module forms
 from app.reservation_app.forms import LoginForm
 
@@ -29,86 +30,39 @@ r = sr.Recognizer()
 pyglet.options['audio'] = ['openal', 'pulse', 'silent']
 
 
-# @reservation_app.route("/login", defaults={'value':0})
 @reservation_app.route("/login", methods=['GET','POST'])
-# @reservation_app.route("/login/<int:value>", methods=['GET','POST'])
-# def login(value):
 def login():
 
-    # initiate the down key presses variable to 3 to keep the mic switched off
-    # downkeypresses = 3
-    #
-    # if request.method == 'POST':
-    #     print("I am here")
-    #     mic_status = request.form['message']
-    #     downkeypresses = int(request.form['downkeypresses'])
-    #     print(mic_status)
-    #
-    # print("Downkeypresses :", downkeypresses)
-    #
-    # # Value = 1 is when the microphone will switch on else, it will return the standard text which is 'Say something'
-    # # if the down key is pressed for more than 2 times, the mic should not be switched on
-    # if (value == 1 and downkeypresses <=2):
-    #     print("I am inside the mic construct")
-    #     # Now get the user input via the microphone
-    #     with sr.Microphone() as source:
-    #         r.adjust_for_ambient_noise(source, duration=1)
-    #         audio = r.listen(source)
-    #         print(audio)
-    #         try:
-    #             text = r.recognize_google(audio)
-    #             print(text)
-    #             return jsonify(message=text, downkeypresses=downkeypresses)
-    #         except Exception as e:
-    #             return render_template('reservation_app/login.html', message='Please try again! ' + str(e))
-    #             #return jsonify(message="Please try again!" + str(e))
-
-
-    # if current_user.is_authenticated:
-    #     return redirect(url_for('reservation.index'))
-    # form = LoginForm()
-    #
-    # if form.validate_on_submit():
-    #     username = form.username.data
-    #     password = form.password.data
-    #     username = username.lower()
-    #     password = password.lower()
-    #     try:
-    #         user = Users.query.filter_by(username=username).first()
-    #     # get user id, name and the password from the database
-    #
-    #         get_db_user_name = user.username
-    #         get_db_user_password = user.password
-    #         # validate both username and password matches with the db username and db password
-    #         if (username == get_db_user_name and password == get_db_user_password):
-    #             login_user(user, remember=None)
-    #             next_page = request.args.get('next')
-    #             print(next_page)
-    #             if not next_page or url_parse(next_page).netloc != '':
-    #                 next_page = url_for('reservation.index')
-    #             return redirect(next_page)
-    #             #return redirect(url_for('reservation.index'))
-    #         else:
-    #             flash('Invalid username or password')
-    #             return redirect(url_for('reservation.login'))
-    #     except AttributeError:
-    #         flash('Invalid username or password')
-    #
-    # return render_template('reservation_app/login.html',form=form)
     if current_user.is_authenticated:
         return redirect(url_for('reservation.index'))
     form = LoginForm(request.form)
-    flash(form.errors)
+
     if form.validate_on_submit():
-        user = Users.query.filter_by(username=form.username.data).first()
-        password = user.password
-        if user is None or password is None:
+        username = form.username.data
+        password = form.password.data
+        username = username.lower()
+        password = password.lower()
+
+        if username is None or password is None:
             flash('Invalid username or password')
             return redirect(url_for('reservation.login'))
-        login_user(user, remember=None)
-        return redirect(url_for('reservation.index'))
-    return render_template('reservation_app/login.html', title='Sign In', form=form)
 
+        try:
+            user = Users.query.filter_by(username=username).first()
+        # get user id, name and the password from the database
+
+            get_db_user_name = user.username
+            get_db_user_password = user.password
+            # validate both username and password matches with the db username and db password
+            if (username == get_db_user_name and password == get_db_user_password):
+                login_user(user, remember=None)
+                return redirect(url_for('reservation.index'))
+            else:
+                flash('Invalid username or password')
+                return redirect(url_for('reservation.login'))
+        except AttributeError:
+            flash('Invalid username or password')
+    return render_template('reservation_app/login.html',form=form)
 
 @reservation_app.route("/index", defaults={'value':0})
 @reservation_app.route("/index/<int:value>", methods=['GET','POST'])
@@ -125,6 +79,7 @@ def index(value):
         to_station = request.form['to_station']
         key_presses = request.form['key_presses']
         #value = request.form['value']
+
         print (status_message)
         print ('From station: ', from_station, ' ', 'To station: ', to_station)
 
--- app/static/js/login.js
+++ app/static/js/login.js
@@ -1,76 +1,194 @@
-//declare a global variable
-var down_key_presses = 0;
+            // enable speech recognition
+    // The first thing we need to do is check if the user has access to the API
+    // and show an appropriate error message. Unfortunately, the speech-to-text API is
+    // supported only in Chrome and Firefox (with a flag)
 
-// play audio function - this function will play the audio of the file given in the audio_path
-function fnplayaudio(audio_path){
-       var audio_file = new Audio(audio_path);
-       audio_file.play();
-}
+
+    // The recognition variable will give us access to all the API's methods and properties.
+
+    try {
+      var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+      var recognition = new SpeechRecognition();
+    }
+    catch(e) {
+        console.error(e);
+        $('.no-browser-support').show();
+        $('.app').hide();
+    }
+
+
+    /*-----------------------------
+          Voice Recognition
+    ------------------------------*/
+
+    // If false, the recording will stop after a few seconds of silence.
+    // When true, the silence period is longer (about 15 seconds),
+    // allowing us to keep recording even when the user pauses.
+    recognition.continuous = false;
+
+    // set the speech recognition language
+    recognition.lang = 'en-US';
+
+    // set the maximum alternatives
+    recognition.maxAlternatives = 1;
+
+    // set a global variable transcript that holds the speech to text object
+    var transcript = "";
+
+    //declare a global variable
+    // we will be triggering the start and stop of speech recognition using the down arrow key
+    // hence, the following variable
+    var down_key_presses = 0;
+
+    // This block is called every time the Speech APi captures a line.
+    recognition.onresult = function(event) {
+
+      // event is a SpeechRecognitionEvent object.
+      // It holds all the lines we have captured so far.
+      // We only need the current one.
+      //var current = event.resultIndex;
+        var current = event.results.length - 1;
+
+      // Get a transcript of what was said.
+      transcript = event.results[current][0].transcript;
+      console.log(transcript);
+
+      if (down_key_presses == 1){
+          $("#username").val(transcript);
+          readOutLoud('Thank you. Press down arrow key to proceed');
+      }
+
+      if (down_key_presses == 2){
+          $("#password").val(transcript);
+          readOutLoud('Thank you. Press down arrow key to proceed');
+      }
+      //return transcript;
+
+      // // Add the current transcript to the contents of our Note.
+      // // There is a weird bug on mobile, where everything is repeated twice.
+      // // There is no official solution so far so we have to handle an edge case.
+      // var mobileRepeatBug = (current == 1 && transcript == event.results[0][0].transcript);
+      //
+      // if(!mobileRepeatBug) {
+      //   noteContent += transcript;
+      //   noteTextarea.val(noteContent);
+      // }
+    };
+
+
+    // function to convert text into speech
+    function readOutLoud(message) {
+      var speech = new SpeechSynthesisUtterance();
+
+      // Set the text and voice attributes.
+      speech.text = message;
+      speech.volume = 1;
+      speech.rate = 1;
+      speech.pitch = 1;
+
+      window.speechSynthesis.speak(speech);
+    }
+
 
 // once the document is ready, wait for three seconds before playing the welcome message
     $(document).ready(function(){
 
+    // play audio function - this function will play the audio of the file given in the audio_path
+    function fnplayaudio(audio_path){
+           var audio_file = new Audio(audio_path);
+           audio_file.play();
+    }
+
+    function playbeepsound(){
+        fnplayaudio("../../static/audio/beep.wav");
+    }
+
+
         // set time out will hold the message to be played for three seconds before playing it. a value of 3000 indicates three seconds
         setTimeout(function(){
-            fnplayaudio("../../static/audio/welcome_audio.wav");
-        }, 3000);
+            // fnplayaudio("../../static/audio/welcome_audio.wav");
+            readOutLoud('Welcome to the reservation portal');
+        }, 2000);
 
-        $("#username").focus();
-
-        setTimeout(function(){
-            fnplayaudio("../../static/audio/provide_login_credentials_audio.wav");
-        }, 8000);
+        // $("#username").focus();
 
         setTimeout(function(){
-            fnplayaudio("../../static/audio/login_submit_downkey_audio.wav")
-        }, 12000);
+            // fnplayaudio("../../static/audio/provide_login_credentials_audio.wav");
+            readOutLoud('Please provide your login credentials');
+        }, 7000);
 
-        $('input').keydown(function(e) {
+        setTimeout(function(){
+            // fnplayaudio("../../static/audio/login_submit_downkey_audio.wav")
+            readOutLoud('Press down arrow key to proceed');
+        }, 11000);
+
+        var down_key_presses_mod = 0;
+
+        $('body').keydown(function(e) {
     if (e.keyCode==40) {
 
         e.preventDefault();
         down_key_presses += 1;
+        console.log('down key pressed : ' + down_key_presses);
+        console.log('down key press mod : ' + down_key_presses_mod);
 
         var password_value = $("#password").val();
         var username_value = $("#username").val();
 
-        if (down_key_presses == 1) {
+        down_key_presses_mod = down_key_presses % 3;
+
+        if (down_key_presses_mod == 1) {
             $("#username").focus();
-            if (!username_value) {
-                fnplayaudio("../../static/audio/username_audio.wav");
+              if (!username_value) {
+                var username_speech_text = $("#username").attr("speech-unset-text") + " " + "after the beep";
+                readOutLoud(username_speech_text);
+                setTimeout(function(){
+                    recognition.start();
+                    playbeepsound();
+                    console.log("speech recognition started");
+                },3000);
             }
-        }
+            else{
+                var username_speech_text = $("#username").attr("speech-set-text") + ' ' + $("#username").val();
+                readOutLoud(username_speech_text);
+              }
+        } // end of if down_key_presses
 
+        if (down_key_presses_mod == 2) {
+            $("#password").focus();
+              if (!password_value) {
+                var password_speech_text = $("#password").attr("speech-unset-text") + " " + "after the beep";
+                readOutLoud(password_speech_text);
+                setTimeout(function(){
+                    recognition.start();
+                    playbeepsound();
+                    console.log("speech recognition started");
+                },3000);
+            }
+            else{
+                var username_speech_text = "Password is provided";
+                readOutLoud(username_speech_text);
+              }
+        } // end of if down_key_presses = 2
 
-        // send a post request to the backend indicating the server to switch on the mic
-        $.post("/reservation/login/1",
-            {
-                message: "mic is on",
-                downkeypresses: down_key_presses,
-                username: username_value,
-                password: password_value
-            }, function(data){
-                if (data.downkeypresses == 1){
-                    $("#username").val(data.message);
-                    $("#password").focus()
-                    if (!password_value) {
-                        fnplayaudio("../../static/audio/pressing_downkey_password_audio.wav");
-                    }
-                }
-                if (data.downkeypresses == 2){
-                    console.log("password response is :" + data.message)
-                    $("#password").val(data.message);
-                    fnplayaudio("../../static/audio/login_submit_downkey_audio.wav")
-                }
-            });
+        if (down_key_presses_mod == 3) {
+            $("#password").focus();
+              if (!password_value) {
+                var password_speech_text = $("#password").attr("speech-unset-text");
+                readOutLoud(password_speech_text);
+                setTimeout(function(){
+                    recognition.start();
+                    console.log("speech recognition started");
+                },2000);
+            }
+            else{
+                var username_speech_text = "Password is set";
+                readOutLoud(username_speech_text);
+              }
+        } // end of if down_key_presses = 2
 
-
-        if(down_key_presses == 3){
-            $("#submit").focus();
-           // fnplayaudio("../../static/audio/login_submit_downkey_audio.wav")
-        }
         console.log("password given is: " + password_value);
-        console.log("username give is : " + username_value);
+        console.log("username given is : " + username_value);
         console.log('down_key_pressed:' + down_key_presses);
     }
 
